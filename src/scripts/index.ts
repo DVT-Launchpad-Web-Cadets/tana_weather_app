452e4326a7410fae0f93ea1c79338982f527e423
tanadendere <tanadendere@gmail.com>
2024-03-01 14:50:09 +0200

    fix: :bug: Removing the redundant subscriptions
    Removed the subscription function and now only subscribe to the observable once

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -6,13 +6,15 @@ import { majorCitiesData } from "../coordinates.ts";
 
 // GLOBAL VARIABLES
 let currentCity = majorCitiesData[0];
-console.log("Current city: ", currentCity.cityName);
-
 const map = initilizeMap(currentCity);
 
 // Loading current location on start up
 weatherFetchRequest$.next(currentCity);
-subscribeToWeatherAPI(currentCity);
+
+weatherResultSet$.subscribe((res) => {
+  setDOM(res, currentCity.cityName);
+  updateMap(map, currentCity);
+});
 
 // map listeners
 const mapDiv = document.getElementById("map-container") as HTMLElement;
@@ -35,52 +37,21 @@ const majorCities: HTMLElement[] = Array.from(getMajorCities);
 
 for (const city of majorCities) {
   city.addEventListener("click", function () {
-    console.log("this is running");
     const cityData = getCityCoords(city.innerText);
 
     if (Object.keys(cityData).length === 0) {
       throw new Error(`No data for ${city.innerText}`);
     }
+    currentCity = cityData;
     weatherFetchRequest$.next(cityData);
-    subscribeToWeatherAPI(cityData);
-    // weatherObservable$.unsubscribe();
   });
 }
 
 export function getCityCoords(city: string): ICityData {
   for (const majorCity of majorCitiesData) {
     if (majorCity.cityName === city) {
-      console.log(majorCity.cityName);
       return majorCity;
     }
   }
   return {} as ICityData;
 }
-
-export function subscribeToWeatherAPI(cityData: ICityData): void {
-  // console.log("hello 2");
-
-  // weatherFetchRequest$.next(cityData);
-
-  // console.log("hello 3");
-  console.log("in the subscribe to weather api function");
-
-  weatherResultSet$.subscribe((res) => {
-    console.log("subscribed");
-    console.log(res);
-    setDOM(res, cityData.cityName);
-    updateMap(map, cityData);
-  });
-
-  // console.log("hello 4");
-}
-
-// export function callTheWeatherAPI(cityData: ICityData): void {
-//   getWeatherData(cityData.longitude, cityData.latitude)
-//     .then((res) => {
-//       // mapping of results
-//       setDOM(res, cityData.city);
-//       updateMap(map, cityData);
-//     })
-//     .catch(console.error);
-// }
