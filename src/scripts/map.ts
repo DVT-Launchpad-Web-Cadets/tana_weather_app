7617d40e38ddf0ffa4130f5ec642a7eeed60715d
tanadendere <tanadendere@gmail.com>
2024-03-01 14:59:47 +0200

    fix: :bug: Removing redundant subscription and observables in map function

--- src/scripts/map.ts
+++ src/scripts/map.ts
@@ -1,6 +1,6 @@
 import { map as createMap, latLng, marker, tileLayer, Map } from "leaflet";
-import { fromEvent } from "rxjs";
-import { subscribeToWeatherAPI } from "./index.ts";
+import { weatherFetchRequest$ } from "./api.ts";
+
 import { ICityData } from "../models/cityData";
 import { mapDisplayToggle } from "./dom.ts";
 
@@ -20,8 +20,6 @@ export function initilizeMap(currentCity: ICityData): Map {
     zoom: 13,
   });
 
-  // myMap.setView([currentCity.latitude, currentCity.longitude], 13);
-
   tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
@@ -29,8 +27,6 @@ export function initilizeMap(currentCity: ICityData): Map {
   marker(latLng(currentCity.latitude, currentCity.longitude))
     .addTo(myMap)
     .openPopup();
-
-  console.log("Map is initialised.");
   return myMap;
 }
 
@@ -40,18 +36,13 @@ export function mapListeners(
   sevenDayDiv: HTMLElement,
   openMap: HTMLElement
 ): void {
-  // openMap.addEventListener("click", () =>
-  //   mapDisplayToggle(mapDiv, sevenDayDiv)
-  // );
-
-  const mapButton$ = fromEvent(openMap, "click");
-  mapButton$.subscribe(() => {
-    mapDisplayToggle(mapDiv, sevenDayDiv);
-  });
+  openMap.addEventListener("click", () =>
+    mapDisplayToggle(mapDiv, sevenDayDiv)
+  );
 
   map.on("click", onMapClick);
 
-  function onMapClick(e: any) {
+  function onMapClick(e: { latlng: { lat: number; lng: number } }) {
     let longitude = Math.round(e.latlng.lat * 1000) / 1000;
     let latitude = Math.round(e.latlng.lng * 1000) / 1000;
     const cityData: ICityData = {
@@ -59,7 +50,7 @@ export function mapListeners(
       latitude: latitude,
       cityName: "Selected Location",
     };
-    subscribeToWeatherAPI(cityData);
+    weatherFetchRequest$.next(cityData);
     mapDiv.style.display = "none";
     sevenDayDiv.style.display = "flex";
   }
